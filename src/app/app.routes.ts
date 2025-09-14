9265a753dbcd8fb522b1ed735327a7cb24b5de7b
Pavlo Dudnyk <pablokumar1993@gmail.com>
2025-09-14 22:27:17 +0200

    WIP: The routing and the three main components for testing routing are created.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoadDataComponent } from './components/load-data/load-data.component';
+import { AnalyzeDataComponent } from './components/analyze-data/analyze-data.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'load-data', component: LoadDataComponent },
+  { path: 'analyze-data', component: AnalyzeDataComponent },
+  { path: '**', redirectTo: '/home' }
+];
---
+++ src/app/components/analyze-data/analyze-data.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analyze-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './analyze-data.component.html',
+  styleUrl: './analyze-data.component.css'
+})
+export class AnalyzeDataComponent {
+
+}
